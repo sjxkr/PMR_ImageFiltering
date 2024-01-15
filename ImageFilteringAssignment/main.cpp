f613194cc7d5af3094ed70b2d88cd745167dee71
Shahir Jagot <jagot9@hotmail.com>
2024-01-15 12:39:09 +0000

    Init testing of  smooth function

--- ImageFilteringAssignment/main.cpp
+++ ImageFilteringAssignment/main.cpp
@@ -23,13 +23,31 @@ int main()
 	ofstream fout;	//output file
 	char inputFilename[] = "placeholderfilename.txt";	//intialised char array
 	int Info[] = { 0,0,0 }; // used in read header function
-	vector<vector<Pixel>> image;	// Pixel data of input image
+	vector<vector<Pixel>> image;	// Image data
 
-	cout << Info[0] << endl;	//debugging
-	cout << inputFilename << endl;	//debugging
+	cout << Info[0] << endl;	//only for debugging
+	cout << inputFilename << endl;	//only for debugging
 
 	openIOFiles(fin, fout, inputFilename);		//Open input and output file
 	convertP6ToP3(fin, fout, image, Info);
+
+	/*
+	Testing section to understand given smooth function
+	*/
+
+	int h = image.size();	//get iamge size
+	int w = image[0].size();
+
+	cout << "The image height is " << h << endl;
+	cout << "The image width is " << w << endl;
+
+	Pixel first;
+	int i = 0;
+	int j = 1;
+
+	first = image[i][j];
+
+	cout << "The selected pixel colour is " << first << endl;
 
 	return(0);
 }
