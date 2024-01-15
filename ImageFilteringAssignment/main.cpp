c4c2b0dacf8b903cbf327607a26cd964c7ab34ad
Shahir Jagot <jagot9@hotmail.com>
2024-01-15 16:53:29 +0000

    tested pixel and kernel multiplication. Issue with multiplication of negative scalars. to be resolved.

--- ImageFilteringAssignment/main.cpp
+++ ImageFilteringAssignment/main.cpp
@@ -12,6 +12,7 @@
 #include "ImageFunctions.h"
 #include "Pixel.h"
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -24,6 +25,14 @@ int main()
 	char inputFilename[] = "placeholderfilename.txt";	//intialised char array
 	int Info[] = { 0,0,0 }; // used in read header function
 	vector<vector<Pixel>> image;	// Image data
+	
+	vector<vector<int>> filter{
+		{-1,-1,-1},
+		{-1,9,-1},
+		{-1,-1,-1}
+	};		//  3x3 filter kernel	
+
+	vector<Pixel>temp;
 
 	cout << Info[0] << endl;	//only for debugging
 	cout << inputFilename << endl;	//only for debugging
@@ -44,22 +53,41 @@ int main()
 	Pixel first;
 	Pixel sum;
 	Pixel product;
-	int i = 0;
-	int j = 0;
+	int i = 1;
+	int j = 1;
 
 	first = image[i][j];
 
-	cout << "The selected pixel colour is " << first << endl;
+	cout << "The selected pixel is " << first << endl;
+	
+	/* Testing Overloaded operators multiplication and addition
+	* What i want to achieve here is to apply the filter to one pixel, sum the values, validate them and write them to a file	
+	*/
 
-	/* Testing Overloaded operators multiplication and addition */
-	product = image[i][j] * 9;	
+	// initialise filter kernel with values
+	
+	
+	for (int k = 0; k < 3; k++)
+	{
+		for (int l = 0; l < 3; l++)
+		{
+			product = (image[k][l] * filter[k][l]);
+			temp.push_back(product);
+			//cout << filter[k][l] << endl;	//print filter values one by one row by row
+		}
+	}
+	
+	//print products for loop
 
-	cout << "The product is equal to " << product << endl;
+	for (int a = 0; a < 9; a++)
+	{
+		cout << "value " << a + 1 << " is: " << temp[a] << endl;
+	}
 
 	sum = image[i][j] + image[i][j + 1];
 
 	cout << "The sum of " << image[i][j] << " and " << image[i][j + 1] << " is " << sum << endl;
-
+	
 
 	return(0);
 }
