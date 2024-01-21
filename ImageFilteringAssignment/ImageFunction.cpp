5e98b17d9609cb19a811861992680fee153d0817
sjxkr <jagot9@hotmail.com>
2024-01-21 18:35:02 +0000

    Revert "Revert "fix - formatting and presentation""
    This reverts commit fb82ca0825afb9bb3e7d903fb02578c2fffb2b0f.

--- ImageFilteringAssignment/ImageFunction.cpp
+++ ImageFilteringAssignment/ImageFunction.cpp
@@ -57,12 +57,13 @@ void sharpen(vector<vector<Pixel> >& image)
 	// define variables
 	int h = image.size();
 	int w = image[0].size();
+	vector<vector<Pixel>> imageOut = image;		// output image intialised as input image
 	ofstream out;
 	char outFilename[MAXLEN];
 	char comment[MAXLEN] = "# Sharpen filter has been applied this image";
 	int r = 0, g = 0, b = 0;
-	int maxColor = 0; // change this to be read from header, imageinfo variable.
-	vector<vector<Pixel>> imageOut = image;		// output image intialised as input image
+	int maxColor = 0;
+	
 	
 	// initialise convolution matrix
 	vector<vector<int>> filter{
@@ -74,7 +75,7 @@ void sharpen(vector<vector<Pixel> >& image)
 	// open file for filtered image data
 
 	// prompt user for filename
-	cout << "enter filename for filtered image:" << endl;
+	cout << "enter filename without extension and press enter:" << endl;
 	cin >> outFilename;
 
 	// add suffix and file extension
